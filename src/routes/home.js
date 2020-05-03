c4185adfff2014931714c32000ca1cef238e0732
lucascprazeres <olucascostadp@gmail.com>
2020-05-03 16:11:42 -0300

    basic file architecture + express

---
+++ src/routes/home.js
@@ -0,0 +1,9 @@
+//  Home routes
+import { Router } from 'express';
+import homeController from '../controllers/home';
+
+const router = new Router();
+
+router.get('/', homeController.index);
+
+export default router;
