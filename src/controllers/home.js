800e24838ab78dddc5fe3c352219c913cc39b1cf
lucascprazeres <olucascostadp@gmail.com>
2020-05-05 15:14:22 -0300

    student routes index

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,17 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Outro',
-      sobrenome: 'Nome',
-      email: 'anotherEmail@example.com',
-      idade: 28,
-      peso: 55.0,
-      altura: 1.70,
-    });
-
-    res.json(novoAluno);
+    res.json('Home');
   }
 }
 
---
+++ src/routes/aluno.js
@@ -0,0 +1,9 @@
+//  Home routes
+import { Router } from 'express';
+import alunoController from '../controllers/aluno';
+
+const router = new Router();
+
+router.get('/', alunoController.index);
+
+export default router;
